7e647554aa57bc112f4513b912a74c64ec402700
ccplatz <mail@ccplatz.de>
2023-12-24 14:46:05 +0100

    refactoring to OOP approach

--- app/main.js
+++ app/main.js
@@ -6,202 +6,276 @@ const inputElem = document.getElementById('input');
 const htmlListElem = document.getElementById('htmlList');
 const devideHomeAndAwayElem = document.getElementById('devideHomeAndAway');
 const showDateElem = document.getElementById('showDates');
-const mappings = {
-    'M-ML': '1. Herren',
-    'F-KK-1': '1. Damen',
-    'M-2KK-1': '2. Herren',
-    'wA-MK': 'Weibliche A-Jugend',
-    'wC-MK2': 'Weibliche C-Jugend',
-    'wD-ML2': 'Weibliche D-Jugend',
-    'wE-ML4': 'Weibliche E-Jugend',
-    'mA ML VR': 'Männliche A-Jugend',
-    'mC-MK2': 'Männliche C-Jugend',
-    'mD-MK3': 'Männliche D-Jugend',
-    'mE-ML4': 'Männliche E-Jugend',
-    'F-Freu': 'Freundschaftsspiele Damen',
-};
 
-const asHtmlList = () => htmlListElem.checked;
-const withDate = () => showDateElem.checked;
-const devideHomeAndAway = () => devideHomeAndAwayElem.checked;
-const emptyOutput = () => (outputElem.value = '');
-const showOutput = (output) => (outputElem.value = output);
-
-const buildGameDataObject = function (gameData) {
-    return {
-        team: gameData[0].trim(),
-        id: gameData[1],
-        date: gameData[2],
-        home: gameData[4],
-        guest: gameData[5],
-        result: gameData[6],
+class App {
+    #games;
+    #mappings = {
+        'M-ML': '1. Herren',
+        'F-': '1. Damen',
+        'M-2KK-1': '2. Herren',
+        wA: 'Weibliche A-Jugend',
+        wC: 'Weibliche C-Jugend',
+        wD: 'Weibliche D-Jugend',
+        wE: 'Weibliche E-Jugend',
+        mA: 'Männliche A-Jugend',
+        mC: 'Männliche C-Jugend',
+        mD: 'Männliche D-Jugend',
+        mE: 'Männliche E-Jugend',
     };
-};
 
-const mapTeamDescription = function (gameDataObject) {
-    let teamDescription = '';
-    Object.entries(mappings).forEach(([key, value]) => {
-        if (gameDataObject.team == key) {
-            teamDescription = value;
-        }
-    });
-
-    return teamDescription;
-};
-
-const getDateFromString = function (dateString) {
-    dateString = dateString.replace('h', '');
-    const [, datePart, time] = dateString.split(', ');
-    const [day, month, yearShort] = datePart.split('.');
-    const year = '20' + yearShort;
-    const [hour, minute] = time.split(':');
-    const parsedDate = new Date(
-        parseInt(year),
-        parseInt(month) - 1,
-        parseInt(day),
-        parseInt(hour),
-        parseInt(minute)
-    );
-
-    return parsedDate;
-};
-
-const getFormattedDateString = function (dateObject) {
-    return new Intl.DateTimeFormat('de-DE', {
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-    }).format(dateObject);
-};
-
-const buildGameString = function (gameDataObject) {
-    let game = '';
-    const date = withDate()
-        ? getFormattedDateString(gameDataObject.date) + ', '
-        : '';
-    if (gameDataObject.result === ':') {
-        game = `${gameDataObject.team}: ${date}${gameDataObject.home} gg. ${gameDataObject.guest}`;
-    } else {
-        game = `${gameDataObject.team}: ${date}${gameDataObject.home} gg. ${gameDataObject.guest}, ${gameDataObject.result}`;
+    constructor() {
+        processBtn.addEventListener('click', this.#processInput.bind(this));
     }
 
-    return game;
-};
-
-const setTeamDescriptions = function (gameDataArr) {
-    gameDataArr.map((line, key) => {
-        if (line.length < 8 && !(line[0] in mappings)) {
-            line.unshift(gameDataArr[key - 1][0]);
-        }
-    });
-    return gameDataArr;
-};
-
-const setGameType = function (gameDataObject) {
-    if (gameDataObject.home.search(/Sendenhorst/i) > -1) {
-        gameDataObject.type = 'home';
-    } else {
-        gameDataObject.type = 'away';
+    #processInput() {
+        this.#emptyOutputField();
+        const linesArr = this.#getUserInputAsArray();
+        this.#games = this.#processLines(linesArr);
+        let outputText = this.#renderOutput();
+        this.#showOutput(outputText);
     }
-};
 
-const setHomeAndGuest = function (gameDataObject) {
-    if (gameDataObject.home.search(/Sendenhorst/i) > -1) {
-        gameDataObject.home = 'SGS';
-    } else {
-        gameDataObject.guest = 'SGS';
+    #emptyOutputField() {
+        outputElem.value = '';
     }
-};
 
-const processLines = function (linesArr) {
-    const homeGames = [];
-    const awayGames = [];
-    const allGames = [];
+    #getUserInputAsArray() {
+        const inputText = inputElem.value;
+        return inputText.split('\n');
+    }
 
-    let gameDataArr = linesArr.map((line) => line.split('\t'));
+    #processLines(linesArr) {
+        const homeGames = [];
+        const awayGames = [];
+        const allGames = [];
 
-    // Skip if no game ID exists
-    gameDataArr = gameDataArr.filter((line) => {
-        return line[1];
-    });
+        const isHomeGame = (game) => game.type === 'home';
 
-    // Copy team description from previous line if team has more than one game per week
-    gameDataArr = setTeamDescriptions(gameDataArr);
+        // Split every line by the tab symbol
+        let gameDataArr = linesArr.map((line) => line.split('\t'));
 
-    gameDataArr.forEach((line) => {
-        const gameDataObject = buildGameDataObject(line);
+        // Skip if no game ID exists
+        gameDataArr = gameDataArr.filter((line) => {
+            return line[1];
+        });
 
-        gameDataObject.team = mapTeamDescription(gameDataObject);
-        gameDataObject.date = getDateFromString(gameDataObject.date);
-        setGameType(gameDataObject);
-        setHomeAndGuest(gameDataObject);
+        // Copy team description from previous line if team has more than one game per week
+        gameDataArr = this.#setTeamDescriptions(gameDataArr);
 
-        if (gameDataObject.type === 'home') {
-            homeGames.push(gameDataObject);
-        } else {
-            awayGames.push(gameDataObject);
+        gameDataArr.forEach((lineArr) => {
+            const game = this.#getNewGame(lineArr);
+
+            if (isHomeGame(game)) {
+                homeGames.push(game);
+            }
+
+            if (!isHomeGame(game)) {
+                awayGames.push(game);
+            }
+
+            allGames.push(game);
+        });
+
+        // Sort games by date
+        const gamesArr = [homeGames, awayGames, allGames].map((gameArr) =>
+            gameArr.sort((a, b) => a.date - b.date)
+        );
+
+        return gamesArr;
+    }
+
+    #setTeamDescriptions(gameDataArr) {
+        gameDataArr.map((line, key) => {
+            if (line[0] === '' && !(line[0] in this.#mappings)) {
+                line[0] = gameDataArr[key - 1][0];
+            }
+        });
+        return gameDataArr;
+    }
+
+    #getNewGame(lineArr) {
+        const rawTeam = lineArr[0].trim();
+        const team = this.#mapTeamDescription(rawTeam);
+
+        const rawDate = lineArr[2];
+        const date = this.#getDateObjectFromString(rawDate);
+
+        const home = lineArr[4].trim();
+        const guest = lineArr[5].trim();
+        const result = lineArr[6].trim();
+
+        const game = new Game(team, date, home, guest, result);
+
+        return game;
+    }
+
+    #mapTeamDescription(rawTeam) {
+        let team = '';
+        Object.entries(this.#mappings).forEach(([key, value]) => {
+            if (rawTeam.includes(key)) {
+                team = value;
+            }
+        });
+
+        return team;
+    }
+
+    #getDateObjectFromString(dateString) {
+        dateString = dateString.replace('h', '');
+        const [, datePart, time] = dateString.split(', ');
+        const [day, month, yearShort] = datePart.split('.');
+        const year = '20' + yearShort;
+        if (time) {
+            const [parsedHour, parsedMinute] = time.split(':');
+            const hour = parseInt(parsedHour);
+            const minute = parseInt(parsedMinute);
+            return new Date(
+                parseInt(year),
+                parseInt(month) - 1,
+                parseInt(day),
+                parseInt(hour),
+                parseInt(minute)
+            );
         }
-        allGames.push(gameDataObject);
-    });
 
-    const processedGames = [homeGames, awayGames, allGames].map((gameArr) =>
-        gameArr.sort((a, b) => a.date - b.date).map(buildGameString)
-    );
+        return new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
+    }
 
-    return processedGames;
-};
+    #renderOutput() {
+        const asHtmlList = () => htmlListElem.checked;
 
-const getListFromGames = function (games) {
-    let htmlText = '<ul>';
-    games.forEach((game) => (htmlText += `<li>${game}</li>`));
-    htmlText += '</ul>';
+        const output = asHtmlList()
+            ? this.#getHtmlOutput(...this.#games)
+            : this.#getTextFromGames(...this.#games);
 
-    return htmlText;
-};
+        return output;
+    }
 
-const getTextFromGames = function (homeGames, awayGames, allGames) {
-    let text = 'Hier die Übersicht der Spiele:\n\n';
+    #getTextFromGames(homeGames, awayGames, allGames) {
+        let text = 'Hier die Übersicht der Spiele:\n\n';
 
-    if (devideHomeAndAway()) {
-        text += 'Heimspiele:\n';
-        text += homeGames.join('\n') + '\n\n';
-        text += 'Auswärtsspiele:\n';
-        text += awayGames.join('\n');
+        if (this.#devideHomeAndAway()) {
+            text += 'Heimspiele:\n';
+            text +=
+                homeGames
+                    .map((game) => game.getGameString(this.#withDate()))
+                    .join('\n') + '\n\n';
+            text += 'Auswärtsspiele:\n';
+            text += awayGames
+                .map((game) => game.getGameString(this.#withDate()))
+                .join('\n');
+
+            return text;
+        }
+
+        text = allGames
+            .map((game) => game.getGameString(this.#withDate()))
+            .join('\n');
 
         return text;
     }
 
-    text = allGames.join('\n');
+    #devideHomeAndAway() {
+        return devideHomeAndAwayElem.checked;
+    }
 
-    return text;
-};
+    #withDate() {
+        return showDateElem.checked;
+    }
 
-const getHtmlOutput = function (homeGames, awayGames, allGames) {
-    let htmlText = '<strong>Hier die Übersicht der Spiele:</strong>\n\n';
+    #getHtmlOutput(homeGames, awayGames, allGames) {
+        let htmlText = '<strong>Hier die Übersicht der Spiele:</strong>\n\n';
 
-    if (devideHomeAndAway()) {
-        htmlText += 'Heimspiele:\n';
-        htmlText += getListFromGames(homeGames) + '\n&nbsp;\n\n';
-        htmlText += 'Auswärtsspiele:\n';
-        htmlText += getListFromGames(awayGames) + '\n';
+        if (this.#devideHomeAndAway()) {
+            htmlText += 'Heimspiele:\n';
+            htmlText += this.#getListFromGames(homeGames) + '\n&nbsp;\n\n';
+            htmlText += 'Auswärtsspiele:\n';
+            htmlText += this.#getListFromGames(awayGames) + '\n';
+
+            return htmlText;
+        }
+
+        htmlText = this.#getListFromGames(allGames);
 
         return htmlText;
     }
 
-    htmlText = getListFromGames(allGames);
+    #getListFromGames(games) {
+        let htmlText = '<ul>\n';
+        games.forEach(
+            (game) =>
+                (htmlText += `<li>${game.getGameString(
+                    this.#withDate()
+                )}</li>\n`)
+        );
+        htmlText += '</ul>';
 
-    return htmlText;
-};
+        return htmlText;
+    }
 
-processBtn.addEventListener('click', function (event) {
-    emptyOutput();
-    const inputText = inputElem.value;
-    const linesArr = inputText.split('\n');
-    const games = processLines(linesArr);
-    let outputText = asHtmlList()
-        ? getHtmlOutput(...games)
-        : getTextFromGames(...games);
-    showOutput(outputText);
-});
+    #showOutput(output) {
+        outputElem.value = output;
+    }
+}
+
+class Game {
+    constructor(team, date, home, guest, result) {
+        this.team = team;
+        this.date = date;
+        this.home = home;
+        this.guest = guest;
+        this.result = result;
+        this.#setGameType();
+        this.#setSgsTeam();
+    }
+
+    get dateString() {
+        const hour = this.date.getHours();
+        const minute = this.date.getMinutes();
+
+        if (hour === 0 && minute === 0) {
+            return new Intl.DateTimeFormat('de-DE', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+            }).format(this.date);
+        }
+
+        return new Intl.DateTimeFormat('de-DE', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+        }).format(this.date);
+    }
+
+    #setGameType() {
+        if (this.home.search(/Sendenhorst/i) > -1) {
+            this.type = 'home';
+        } else {
+            this.type = 'away';
+        }
+    }
+
+    #setSgsTeam(gameDataObject) {
+        if (this.home.search(/Sendenhorst/i) > -1) {
+            this.home = 'SGS';
+        } else {
+            this.guest = 'SGS';
+        }
+    }
+
+    getGameString(withDate = true) {
+        let gameString = '';
+        const date = withDate ? this.dateString + ', ' : '';
+        gameString = `${this.team}: ${date}${this.home} gg. ${this.guest}`;
+        if (this.result !== ':') {
+            gameString += `, ${this.result}`;
+        }
+
+        return gameString;
+    }
+}
+
+const app = new App();
